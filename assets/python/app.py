ff90e0678db6aaffaaf19017cfa4d01fbbd1f60d
Flow-matic <jonathonvinson22@gmail.com>
2022-06-21 19:40:11 +0000

    Added finishing code to app.py file jquery, bootstrap

--- assets/python/app.py
+++ assets/python/app.py
@@ -82,6 +82,19 @@ col7.metric(col7_selection, col7_price, col7_percent)
 col8.metric(col8_selection, col8_price, col8_percent)
 col9.metric(col9_selection, col9_price, col9_percent)
 
+st.header('**All Price**')
+st.dataframe(df)
+
+st.info(' Created by Jonathan Vinson ( Code Institute Student )')
+
+st.markdown(""" 
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""", unsafe_allow_html=True)
+
+
+
 
 
 
