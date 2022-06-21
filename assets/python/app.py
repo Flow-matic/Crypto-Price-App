5c131f44de0118c895a05c74a7a7ced1455506db
Flow-matic <jonathonvinson22@gmail.com>
2022-06-21 18:36:39 +0000

    Added Binance API link

--- assets/python/app.py
+++ assets/python/app.py
@@ -4,3 +4,11 @@ import pandas as pd
 st.markdown('''# **Binance Price App**
 A cryptocurrency price app pulling price data from *Binance API*.
 ''')
+
+st.header('**Selected Prices')
+
+# Load market data from Binance API
+
+df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
+
+# Custom function for rounding values
