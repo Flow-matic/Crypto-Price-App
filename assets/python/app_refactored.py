1fb5356197aa8331a19f5fe4268d315dbbbed48c
Flow-matic <jonathonvinson22@gmail.com>
2022-06-21 19:49:35 +0000

    Added Binance API key

--- assets/python/app_refactored.py
+++ assets/python/app_refactored.py
@@ -4,3 +4,10 @@ import pandas as pd
 st.markdown('''# **Binance Price App**
 A cryptocurrency price app pulling price data from *Binance API*.
 ''')
+
+st.header('**Selected Price**')
+
+# Load market data from Binance API 
+
+df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
+
