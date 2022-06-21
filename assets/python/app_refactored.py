983c515ffc4c090c58f640187655f50dc158fa28
Flow-matic <jonathonvinson22@gmail.com>
2022-06-21 19:59:27 +0000

    Added code for custom function values, if, else, and return statements with crypto price list

--- assets/python/app_refactored.py
+++ assets/python/app_refactored.py
@@ -11,3 +11,23 @@ st.header('**Selected Price**')
 
 df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
 
+# Custom function for rouinding values
+
+def round_value(input_value):
+    if input_value.values > 1:
+        a = float(round(input_value, 2))
+    else:
+        a = float(round(input_value, 8))
+    return a
+
+cryptolist = {
+    'Price 1': 'BTCBUSD',
+    'Price 2': 'ETHBUSD',
+    'Price 3': 'BNBBUSD',
+    'Price 4': 'XRPBUSD',
+    'Price 5': 'ADABUSD',
+    'Price 6': 'DOGEBUSD',
+    'Price 7': 'SHIBBUSD',
+    'Price 8': 'DOTBUSD',
+    'Price 9': 'MATICBUSD'
+}
