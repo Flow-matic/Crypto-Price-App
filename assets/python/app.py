5c3ff85189d17073365dd6cbb29e2c25931ea0b5
Flow-matic <jonathonvinson22@gmail.com>
2022-06-21 19:02:40 +0000

    Addedcode  selection box widget to app.py

--- assets/python/app.py
+++ assets/python/app.py
@@ -20,4 +20,19 @@ def round_value(input_value):
         a = float(round(input_value, 8))
     return a
 
-    
+col1, col2, col3, = st.columns(3)
+
+# Widget (Cryptocurrency selection box)
+
+col1_selection = st.sidebar.selectbox('Price 1', df.symbol, list(df.symbol).index('BTCBUSD') )
+col2_selection = st.sidebar.selectbox('Price 2', df.symbol, list(df.symbol).index('ETHBUSD') )
+col3_selection = st.sidebar.selectbox('Price 3', df.symbol, list(df.symbol).index('BNBBUSD') )
+col4_selection = st.sidebar.selectbox('Price 4', df.symbol, list(df.symbol).index('XRPBUSD') )
+col5_selection = st.sidebar.selectbox('Price 5', df.symbol, list(df.symbol).index('ADABUSD') )
+col6_selection = st.sidebar.selectbox('Price 6', df.symbol, list(df.symbol).index('DOGEBUSD') )
+col7_selection = st.sidebar.selectbox('Price 7', df.symbol, list(df.symbol).index('SHIBBUSD') )
+col8_selection = st.sidebar.selectbox('Price 8', df.symbol, list(df.symbol).index('DOTBUSD') )
+col9_selection = st.sidebar.selectbox('Price 9', df.symbol, list(df.symbol).index('MATICBUSD') )
+
+
+
