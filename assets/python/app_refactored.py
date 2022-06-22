f43764a0a8328a6578330b20395fad79e903557c
Flow-matic <jonathonvinson22@gmail.com>
2022-06-22 20:18:32 +0000

    Added crypto list symbol, percentage, and weighted avg code for Binance price app

--- assets/python/app_refactored.py
+++ assets/python/app_refactored.py
@@ -11,7 +11,7 @@ st.header('**Selected Price**')
 
 df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
 
-# Custom function for rouinding values
+# Custom function for rounding values
 
 def round_value(input_value):
     if input_value.values > 1:
@@ -31,3 +31,24 @@ cryptolist = {
     'Price 8': 'DOTBUSD',
     'Price 9': 'MATICBUSD'
 }
+
+col1, col2, col3 = st.columns(3)
+
+for i in range(len(cryptoList.keys())):
+    selected_crypto_label = list(cryptoList.keys())[i]
+    selected_crypto_index = list(df.symbol).index(cryptoList[selected_crypto_label])
+    selected_crypto = st.sidebar.selectbox(selected_crypto_label, df.symbol, selected_crypto_index, key = str(1))
+    col_df = df[df.symbol == selected_crypto]
+    col_price = round_value(col_df.weightedAvgPrice)
+    col_percent = f'{float(col_df.priceChangePercent)}%'
+    if i < 3:
+        with col1:
+            st.metric(selected_crypto, col_price, col_percent)
+    if 2 < i < 6:
+        with col2:
+            st.metric(selected_crypto, col_price, col_percent)
+    if i > 5:
+        with col3:
+            st.metric(selected_crypto, col_price, col_percent)
+
+
